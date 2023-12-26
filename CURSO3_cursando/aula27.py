de91c91df41d742310cc613003d322117d37125c
yDenBarbosa <denis9195@hotmail.com>
2023-12-25 23:44:44 -0300

    OK

---
+++ CURSO3_cursando/aula27.py
@@ -0,0 +1,23 @@
+"""
+Fatiamento de strings
+ 012345678
+ Olá mundo
+-987654321
+Fatiamento [i:f:p] [::]
+Obs.: a função len retorna a qtd 
+de caracteres da str
+"""
+variavel = 'Olá mundo'
+
+print(len(variavel[3]))
+
+print(variavel[5:])
+print(variavel[:1])
+print(variavel[-8:-1])
+print(variavel[-1:-10:-1])
+print(variavel[-1:-6:-1])
+print(variavel[::-1])
+print(variavel[::-1])
+print(variavel[::-1])
+print(variavel[::-1])
+
